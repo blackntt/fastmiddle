99fbe1383c52886aaeb2f7c59a4d8c0d6ba4715b
Toan Nguyen <blackntt@gmail.com>
2020-06-15 20:48:05 +0700

    fix(headers): allow all headers

--- cors-middleware.go
+++ cors-middleware.go
@@ -15,7 +15,7 @@ func (m CORSMiddleWare) Apply(next fasthttp.RequestHandler) fasthttp.RequestHand
 	corsAllowCredentials := "true"
 	newHandler := func(ctx *fasthttp.RequestCtx) {
 		ctx.Response.Header.Set("Access-Control-Allow-Credentials", corsAllowCredentials)
-		ctx.Response.Header.Set("Access-Control-Allow-Headers", corsAllowHeaders)
+		ctx.Response.Header.Set("Access-Control-Allow-Headers", string(ctx.Request.Header.Peek("Origin")))
 		ctx.Response.Header.Set("Access-Control-Allow-Methods", corsAllowMethods)
 		ctx.Response.Header.Set("Access-Control-Allow-Origin", corsAllowOrigin)
 		next(ctx)
